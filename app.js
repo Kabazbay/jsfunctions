b3e70206d2ca7d9fee7227faf99adcb44129edca
Kabazbay <kabazbay98@gmail.com>
2026-03-18 19:51:47 +0100

    myfirstcommit

---
+++ app.js
@@ -0,0 +1,29 @@
+const math = require("./math");
+console.log(math.add(1, 2));
+
+
+// // Basic function
+// function greet(name) {
+//     return "Hello, " + name;
+// }
+
+// console.log(greet("Kachi"));
+
+// // function expression
+// const greet = function(name) {
+//     return "Hello, " + name;
+// }
+
+// console.log(greet("Kachi"));
+
+// // Arrow function
+// const greet = (name) => {
+//     return "Hello, " + name;
+// }
+
+// console.log(greet("Kachi"));
+
+// // Immediately Invoked Function Expression (IIFE)
+// (function() {
+//     console.log("Hello, Kachi");
+// })();
---
+++ math.js
@@ -0,0 +1,19 @@
+
+
+
+// Function to add two numbers
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(1, 2));
+
+
+// Function to subtract two numbers
+function sub(a, b) {
+    return a - b;
+}
+
+console.log(sub(1, 2));
+
+module.exports = { add, sub };
